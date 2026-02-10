1fcaeef9ccbc535473cdd4a788dba49f98e2ce4a
Poueeerr <skubs130@gmail.com>
2026-02-09 22:57:48 -0300

    removing_comments

--- src/comments/repositories/comments.repository.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../../prisma/prisma.service';
-
-@Injectable()
-export class CommentsRepository {
-  constructor(private readonly prisma: PrismaService) {}
-
-  async createComment(postId: number, userId: number, content: string) {}
-}
